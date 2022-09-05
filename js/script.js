4098ec1c129e957835009418f31834b3211862bd
Oleg <olegatortor111@gmail.com>
2022-09-05 21:34:32 +0300

    add rest operator for class in card

--- js/script.js
+++ js/script.js
@@ -136,7 +136,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     
-    // const modalOpen = setTimeout(openModal, 10000);
+    const modalOpen = setTimeout(openModal, 10000);
 
     function showByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight) {
@@ -149,32 +149,35 @@ window.addEventListener('DOMContentLoaded', () => {
     //CLASS START
     const parentCardMenu = document.querySelector('.menu__field .container');
     class CreateCardMenu {
-        constructor(img, title, descr, price) {
+        constructor(img, title, descr, price, ...classes) {
             this.img = img;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
         }
 
         render() {
             const item = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.classes.push('menu__item');
+            }
+            item.classList.add(...this.classes);
             item.innerHTML = `
-                <div class="menu__item">
-                    <img src="img/tabs/${this.img}.jpg" alt="${this.img}">
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
+                <img src="img/tabs/${this.img}.jpg" alt="${this.img}">
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
             `;
             parentCardMenu.append(item);
         }
     }
-    new CreateCardMenu('vegy','Меню "Фитнес"','Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!','229').render();
-    new CreateCardMenu('elite','Меню “Премиум”','В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!','550').render();
-    new CreateCardMenu('post','Меню "Постное"','Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.','430').render();
+    new CreateCardMenu('vegy','Меню "Фитнес"','Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!','229', 'menu__item').render();
+    new CreateCardMenu('elite','Меню “Премиум”','В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!','550', 'menu__item').render();
+    new CreateCardMenu('post','Меню "Постное"','Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.','430', 'menu__item').render();
     //CLASS END
 });
